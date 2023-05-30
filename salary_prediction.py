9677111dffa4012d5f2baee897852fbc567e2746
mehmetyasincirakpy <myasincirak@gmail.com>
2023-05-30 14:05:24 +0300

    Initial commit

---
+++ salary_prediction.py
@@ -0,0 +1,208 @@
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 170)
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+from pandas.core.common import SettingWithCopyWarning
+from sklearn.exceptions import ConvergenceWarning
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+df = pd.read_csv("Datasets/hitters.csv")
+
+
+# GELİŞMİŞ FONKSİYONEL KEŞİFÇİ VERİ ANALİZİ (ADVANCED FUNCTIONAL EDA)
+#############################################
+
+# 1. Genel Resim
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+# 5. Korelasyon Analizi (Analysis of Correlation)
+
+def check_df(dataframe, head=5):
+    print("* * * * * HEAD * * * * *")
+    print(dataframe.head(head))
+    print("* * * * * TAIL * * * * *")
+    print(dataframe.tail(head))
+    print("* * * * * SHAPE * * * * *")
+    print(dataframe.shape)
+    print("* * * * * DTYPES * * * * *")
+    print(dataframe.dtypes)
+    print("* * * * * NA * * * * *")
+    print(dataframe.isnull().sum())
+    print("* * * * * QUANTILES * * * * *")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+check_df(df)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    :param dataframe: Değişken isimleri alınmak istenilen dataframe
+    :param cat_th: int, optional, numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    :param car_th: int, optional, kategorik fakat kardinal değişkenler için sınıf eşik değeri
+    :return:
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+    """
+
+    # cat_cols, cat_but_car
+
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "0"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "0"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "0"]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+#############################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+#############################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+#############################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Salary", col)
+
+
+# 5. Korelasyon Analizi (Analysis of Correlation)
+#############################################
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool_))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show(block=True)
+    return drop_list
+
+
+high_correlated_cols(df, plot=True)
+
+
+# GELİŞMİŞ FONKSİYONEL KEŞİFÇİ VERİ ANALİZİ (ADVANCED FUNCTIONAL EDA)
+#############################################
+
+# 1. Outliers (Aykırı Değerler)
+# 2. Missing Values (Eksik Değerler)
+# 3. Feature Extraction (Özellik Çıkarımı)
+# 4. Encoding (Label Encoding, One-Hot Encoding, Rare Encoding)
+# 5. Feature Scaling (Özellik Ölçeklendirme)
+
+
+#############################################
+# 1. Outliers (Aykırı Değerler)
+#############################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+for col in num_cols:
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
